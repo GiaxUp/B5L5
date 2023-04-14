f74bf6b62aec683bb50021ab8e8d59b0eb4bca7e
Giacomo Della Peruta <giax.95@live.it>
2023-04-14 16:03:02 +0200

    Initial commit

---
+++ src/main/java/com/epicode/B5L5/B5L5Application.java
@@ -0,0 +1,13 @@
+package com.epicode.B5L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B5L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B5L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/epicode/B5L5/B5L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.B5L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B5L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
